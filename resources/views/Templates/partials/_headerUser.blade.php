8f8689f27075011a919adbcfc31251c6e94566fe
AnasMisbahuddin <anas.misbahuddin4869@gmail.com>
2019-05-30 23:09:31 +0800

    add logout admin

--- resources/views/Templates/partials/_headerUser.blade.php
+++ resources/views/Templates/partials/_headerUser.blade.php
@@ -9,14 +9,18 @@
             
             <div class="user-area dropdown float-right">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="user-avatar rounded-circle" src="{{  asset('images/admin.jpg') }}" alt="User Avatar">
+                    <img class="user-avatar rounded-circle" src="{{  asset('/storage/'.Auth::user()->avatar) }}" alt="User Avatar">
                 </a>
 
                 <div class="user-menu dropdown-menu">
                     <a class="nav-link" href="#"><i class="fa fa-user"></i> Administrator</a>
                     <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
                     <a class="nav-link" href="#"><i class="fa fa-cog"></i> Settings</a>
-                    <a class="nav-link" href="#"><i class="fa fa-power-off"></i> Logout</a>
+                    <a class="nav-link" href="{{ route('logout') }}"onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i> Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -165,7 +165,7 @@
         </div>
       </div>
       <div class="row our-team mt-5">
-        <div class="col-6">
+        <div class="col-6 wow fadeInUp">
           <div class="card">
             <div class="card-body">
               <div class="row">
@@ -186,7 +186,7 @@
               <div class="row">
                 <div class="col-3 "><img src="/images/avatarang.png" class="img-circle" alt=""></div>
                 <div class="col-9">
-                  <h5 class="card-title">Anas Misbahuddin</h5>
+                  <h5 class="card-title">Maratus Soleha</h5>
                   <p class="card-text">Tukang buat sistem (backend)</p>
                 </div>
               </div>
@@ -201,7 +201,7 @@
               <div class="row">
                 <div class="col-3 "><img src="/images/avatarang.png" class="img-circle" alt=""></div>
                 <div class="col-9">
-                  <h5 class="card-title">Anas Misbahuddin</h5>
+                  <h5 class="card-title">Asrah</h5>
                   <p class="card-text">Tukang buat sistem (backend)</p>
                 </div>
               </div>
@@ -216,7 +216,7 @@
               <div class="row">
                 <div class="col-3 "><img src="/images/avatarang.png" class="img-circle" alt=""></div>
                 <div class="col-9">
-                  <h5 class="card-title">Anas Misbahuddin</h5>
+                  <h5 class="card-title">Dana Ananda N</h5>
                   <p class="card-text">Tukang buat sistem (backend)</p>
                 </div>
               </div>
@@ -229,7 +229,7 @@
               <div class="row">
                 <div class="col-3 "><img src="images/avatarang.png" class="img-circle" alt=""></div>
                 <div class="col-9">
-                  <h5 class="card-title">Anas Misbahuddin</h5>
+                  <h5 class="card-title">Octafian Christianto</h5>
                   <p class="card-text">Tukang buat sistem (backend)</p>
                 </div>
               </div>
@@ -468,7 +468,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
+    <script src="/lib/wow/wow.min.js"></script>
     {{-- login js --}}
     <script src="template/login/vendor/animsition/js/animsition.min.js"></script>
     <script src="template/login/js/main.js"></script>
